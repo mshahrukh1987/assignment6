07540f7026568d4a4b70d863025aa085d2d121e5
Muhammad Shahrukh Khan <mshahrukh1987@gmail.com>
2020-06-21 16:58:22 +0500

    Assignment6

---
+++ app.js
@@ -0,0 +1,164 @@
+// === Cheapter # 1 and Task # 1:
+
+//alert("Welcome To Our Website!!!!")
+
+// === Cheapter # 1 and Task # 2:
+
+//alert("Error! Please enter a valid passward.")
+
+// === Cheapter # 1 and Task # 3:
+
+//alert("Welcome To JS Land...\n Happy Coading")
+
+// === Cheapter # 1 and Task # 4:
+
+//alert("Welcome to JS Land")
+
+// === Cheapter # 1 and Task # 5:
+
+//console.log ("Hello... I can run JS through my web browser's console")
+
+// === Cheapter # 2 and Task # 1:
+
+//var a = prompt("username", "my name is")
+//alert(a)
+
+// === Cheapter # 2 and Task # 2:
+
+//var a = prompt("Full Name", "My Name")
+//alert(a)
+
+// === Cheapter # 2 and Task # 3:
+
+//alert("Hello World")
+
+// === Cheapter # 2 and Task # 4:
+
+//alert("jhone Doe")
+//alert("15 year Old")
+//alert("certified mobile application Development")
+
+// === Cheapter # 2 and Task # 5:
+
+//escape
+
+// === Cheapter # 2 and Task # 6:
+
+// var a = "My email address is ";
+// var b = "example@example.com";
+// var c = a+b;
+// alert(c)
+
+// === Cheapter # 2 and Task # 7:
+
+// var book = "A smarter way to learn JavaScript"
+// var display = "I am trying to learn from the Book A smarter way to learn javaScript"
+// alert(display)
+
+// === Cheapter # 2 and Task # 8:
+
+// document.write("Yah! I Can Write HTML contect through JavaScript")
+
+// === Cheapter # 2 and Task # 9:
+
+// var a = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(a)
+
+// === Cheapter # 3 and Task # 1:
+
+// var age = "I am 15 Years Old"
+// alert(age);
+
+// === Cheapter # 3 and Task # 2:
+
+// var visit = ("You have visited this site 14 Times")
+// alert(visit)
+
+// === Cheapter # 3 and Task # 3:
+
+// document.write("My birth Year is 1990 </br> Data Type of my declared variable is number")
+
+// === Cheapter # 3 and Task # 4:
+
+// var a = prompt("Visitor’s name", "My Name is")
+// alert(a)
+// var b = prompt("Product title", "Title is")
+// alert(b)
+// var c = prompt("Quantity", "quantity is")
+// alert(c)
+
+// document.write("John Doe ordered 5 T-shirt(s) on XYZ Clothing store")
+
+// === Cheapter # 4 and Task # 1:
+
+// var [a,b,c,d] = [0,1,2,3]
+// console.log(a, b, c, d)
+
+// === Cheapter # 4 and Task # 2:
+
+// 5 legal
+// $, 43, sam, _, userName.
+
+// 5 illegal
+//  , Usar, Javascript, 
+
+// === Cheapter # 4 and Task # 3:
+
+// document.write("<h1> Rules for naming JS variables </h1> </b> </b>")
+// document.write("Variable names can only contain number, $ and _. For example: $my_1stVariable </br> </br>")
+// document.write("Variables must begin with a letter, $ or _. For example $name, _name or name </br> </br>")
+// document.write("Variable names are case sensitive </br> </br>")
+// document.write("Variable names should not be JS keywords </br> </br>")
+
+// === Cheapter # 5 and Task # 1:
+
+// document.write("Sum of 3 and 5 is 8")
+
+// === Cheapter # 5 and Task # 2:
+
+// document.write("Sum of 5 Sub 3 is 2")
+// document.write("Sum of 5 * 3 is 15")
+// document.write("Sum of 6 / 3 is 2")
+
+// === Cheapter # 5 and Task # 4:
+
+// document.write("Total cost of buying 5 Ticket to a Movie is 3000PKR")
+
+
+// === Cheapter # 6 to 9 and Task # 1:
+
+// document.write("Result: </br> The Value of a is: 10 </br> .................................................. </br>")
+// document.write("</br> The value of ++a is: 11 </br> Now the value of a is: 11 </br>")
+// document.write("</br> The value of a++ is: 11 </br> Now the value of a is: 12 </br>")
+// document.write("</br> The value of --a is: 11 </br> Now the value of a is: 11 </br>")
+// document.write("</br> The value of a-- is: 11 </br> Now the value of a is: 10 </br>")
+
+// === Cheapter # 6 to 9 and Task # 2:
+
+// var a = 2;
+// var b = 1;
+// var result = --a - --b + ++b + b--;
+
+// document.write("a is -1 </br> b is 3 </br> result is 2")
+
+// === Cheapter # 6 to 9 and Task # 3:
+
+// var a = prompt("Full Name", "My Name is");
+// alert(a);
+// alert(Thank you for your time);
+
+// === Cheapter # 6 to 9 and Task # 6:
+
+// var subject1 = prompt("Enter subject 1");
+// var subject2 = prompt("Enter subject 2");
+// var subject3 = prompt("Enter subject 3");
+
+// var total = 100;
+
+// var sub1Obtained = prompt("Enter " + subject1 + "Marks" );
+// var sub2Obtained = prompt("Enter " + subject2 + "Marks" );
+// var sub3Obtained = prompt("Enter " + subject3 + "Marks" );
+
+// === Cheapter # 9 to 11 and Task # 1:
+
+
